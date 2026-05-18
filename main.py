4d361ff6b7f25fe0de28b188efb4dfb4c8693488
kaisaloderaud-dot <kaisa.loderaud@tktk.ee>
2026-05-18 14:11:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-
-print(sa oled agy);
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+""" print("Hello, " + first_name + "!") """
+print(f"Hello, {first_name} {last_name} !")
